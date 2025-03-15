dc00772fdb91b4b9d11d832858d7c0fd23889031
rush <rushadmahmed@gmail.com>
2025-03-15 14:10:49 +0530

    Updated Tailwind Config

--- userside/tailwind.config.js
+++ userside/tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: "12px",
+        sm: "14px",
+        base: "16px",
+        lg: "17px",
+        xl: "20px",
+        "2xl": "27px",
+        "3xl": "30px",
+        "4xl": "36px",
+        "5xl": "48px",
+      },
+      fontWeight: {
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+      },
+    },
   },
   plugins: [],
 };

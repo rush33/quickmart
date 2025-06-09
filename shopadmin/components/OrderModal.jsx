920cf3ea383b611a60e9d5add7568d2d48f5657e
rush <rushadmahmed@gmail.com>
2025-06-09 17:26:16 +0530

    status text change

--- shopadmin/components/OrderModal.jsx
+++ shopadmin/components/OrderModal.jsx
@@ -33,7 +33,7 @@ const OrderModal = ({ setIsActive, selectedOrder }) => {
 
   const statusDisplay = {
     PENDING: { text: "Order Pending ⏳", color: "text-yellow-400" },
-    PREPARING: { text: "Preparing Food 🍲", color: "text-yellow-500" },
+    PREPARING: { text: "Preparing Package ", color: "text-yellow-500" },
     READY: { text: "Ready for Pickup 🛵", color: "text-green-500" },
     PICKEDUP: { text: "Picked Up by Driver 🚲", color: "text-green-500" },
     COMPLETE: { text: "Delivered ✅", color: "text-green-500" },

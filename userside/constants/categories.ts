503561e69160d5c55bc029a70637ecea71fc6842
rush <rushadmahmed@gmail.com>
2025-06-23 21:13:37 +0530

    categories update

--- userside/constants/categories.ts
+++ userside/constants/categories.ts
@@ -1,41 +1,11 @@
 export const categories = [
   {
-    title: "Grocery & Kitchen",
+    title: "Categories",
     items: [
       {
-        name: "Dairy, Bread & Eggs",
+        name: "Vegetables",
         image:
-          "https://media-hosting.imagekit.io//cf2cb61303274276/istockphoto-1363070690-612x612.jpg?Expires=1836828416&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=aRsXziIzsXf2g~P7hEI9g-ow-~vqP14tXI3HLpsSnb5U4DzZp6VrJdrVe-1amB-6ek0JGvHNodR2JG4JR7hd5E8TZkqAMCgu7FksfNfs2xLPWpFRSYH~KDpbohch85OFFGo4bWaAH04BSGtaKngnrQ2cCZWhLDaM2LrXHMpJUEQhgiThvrNLiPVfeI0sTPHGKz3a~Q3XAheUReMBnyvnMLYcX6dkGL9125UDYcbmHFAfwtyvdSSprguSeADhx0o9NKGIa7b4mfXaJsLz0lFLL4JfHyDHtak7Lvcm-HXu8vX-8E-1iUqpc2E6LTxtxeSueKFeA1XqLyhFjjivmkvi1Q__",
-      },
-      {
-        name: "Atta, Rice, Oil & Dals",
-        image:
-          "https://media-hosting.imagekit.io//0427bf7409f54e01/FLOURRICE-20200819-153805.webp?Expires=1836828544&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=VeKN78dZdJoIvjXeDtT-Y05J1NVdxWXu0~PzbczftlTminXI~2CfWTgZH2GZRTt~zN0M4HmH0BqcRmrL9Tofe7-6yy4Ge9~0tUJiub4FlcXFlvb002g6vt5teGG5GclDeffLIfgZqe6pUdbZusVfhhe8nKNr0f1BW9DgWx2dmpiFhuxyxqFN81W~gZJWI-18-YeUk5myuowQ9Mnd3TmU4PVYZcpf-YklrcEhrhBdW6x8bUtL5lREXkvz9PQeChmx06y0QVITze0pTo~lFaSUr2Em18aJTv7lvQ-n51FQsES7pjoSazwCC4oXMQnitZFriPv7Rg45ZAEOw7BaFzPaTQ__",
-      },
-      {
-        name: "Meat, Fish & Eggs",
-        image:
-          "https://media-hosting.imagekit.io//8d4ccc30b8e84390/depositphotos_79046312-stock-photo-close-up-of-different-food.jpg?Expires=1836828631&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Sokx9S3KUSrdHixCvKvVB7PyheIRsTAAISiCDNa0I-13s7cYqSyL9tyJ-hFdMbrDMnPsN2NjYQFJkeaEyp06WOjCR8MoIiYi~nmjswhBR1jsF7yFwJndMfnIpSHTc5ib-oQhMhGWKIRpBI~bRV976EhkGWdjhMXrn8WtdxWXTT2rvhHKVSCIa9DBdcj4R4iYADiKYGVG1BufN9Dy3kziXFHrkLBeH4LFn9dZr2~y4bEJ7YU~jM964DU0FIXVi8osLi7CNzvttzkyjiXW7Ra-jy9WaaPKkjquGsNFLSWBji3iQXXGBnaPd2h1r8V1Tsw~M1NFCFTqgP0ybMyJnxnD1g__",
-      },
-      {
-        name: "Breakfast & Sauces",
-        image:
-          "https://media-hosting.imagekit.io//c19aac5961544060/Breakfast-cereals-mobile.webp?Expires=1836828806&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Gh3qdn7ffOZRRouf~0562faaHA7222QA0lHFddnvPxMZXFzSMENVQJ4lgh1worAKW~mgEEIHbmn-wKckBaL6iyLWaErkhvuGUgWGo7nHqvQXtsfaaXpXOb9Ai6O260yTlWFqpFLYZem9~JJEVDFrlUH1EG2kcNKcgxZBGKoEbIDjzVSQnBrL3v7LMLb1GdCKOSNC56Uxn3nt9mx8oWo7iTtGE676omuydd3SDC5K-UFcj509ARLFZz0TcERjt9gBA9WUwCmdE3RRuR788JoExcMYIL7WLKQZgq3tl3Ipu89tkNisTWxM4ibwU0KdMbJTu6ho9IAV36ciOHBxuii5PA__",
-      },
-    ],
-  },
-  {
-    title: "Snacks & Drinks",
-    items: [
-      {
-        name: "Ice Creams & More",
-        image:
-          "https://ik.imagekit.io/jtfemdkjq/71ku+fYqReL._AC_UF350,350_QL80_.jpg?updatedAt=1742221093772",
-      },
-      {
-        name: "Cold Drinks & Juices",
-        image:
-          "https://ik.imagekit.io/jtfemdkjq/coca-cola.jpg?updatedAt=1742221273190",
+          "https://t3.ftcdn.net/jpg/01/47/51/60/360_F_147516063_hCXI8VUIdBYud0B0hhS3Yo5CFTT1a4g8.jpg",
       },
       {
         name: "Snacks",
@@ -43,9 +13,19 @@ export const categories = [
           "https://ik.imagekit.io/jtfemdkjq/poznan-poland-oct-packets-lay-s-potato-chips-popular-american-brand-founded-owned-pepsico-packets-lays-105763625.webp?updatedAt=1742221430386",
       },
       {
-        name: "Biscuits & Cookies",
+        name: "Frozen Food",
         image:
-          "https://ik.imagekit.io/jtfemdkjq/cookies-versus-biscuits-e1528207091843.jpg?updatedAt=1742221525191",
+          "https://images.unsplash.com/photo-1651383140368-9b3ee59c2981?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJvemVuJTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D",
+      },
+      {
+        name: "Dairy",
+        image:
+          "https://www.shutterstock.com/image-photo/guwahati-assam-india-july-09-600nw-1776012053.jpg",
+      },
+      {
+        name: "Home Care",
+        image:
+          "https://cdn.shopaccino.com/refresh/articles/blog-homecare-14-12-2022-691340_l.png?v=531",
       },
     ],
   },
